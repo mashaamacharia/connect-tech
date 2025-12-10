5cd80b28da34de6052c3c2dc8e595f4012cc53d0
kenvindross <kenvindross@gmail.com>
2025-12-10 10:09:17 +0300

    fixed favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -80,7 +80,7 @@ export const metadata: Metadata = {
     icon: [
       { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
       { url: "/favicon-32x32.jpg", sizes: "32x32", type: "image/png" },
-      { url: "/favicon.jpg", sizes: "any" },
+      { url: "/favicon.png", sizes: "any" },
     ],
     apple: [{ url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" }],
     other: [{ rel: "mask-icon", url: "/safari-pinned-tab.svg", color: "#1b99a7" }],
